4e377d55f6fc75e949ed0367840eed78337fa066
july1012 <113192987+july1012@users.noreply.github.com>
2023-10-12 13:16:59 -0400

    dfasfh

--- bear.js
+++ bear.js
@@ -3,37 +3,43 @@ const bears = [
         type: "polar bear ",
         location: "arctic",
         color: "white",
-        diet: ["ringed seals, walrus, narwhals, beluga"],
+        diet: ["ringed seals", "walrus", "narwhals", "beluga"],
         endangered: true,
     },
     {
         type:"grizzly bear",
         location:"Alaska, Wyoming, Montana, Idaho ",
         color: "brown ",
-        diet: ["fruits, roots, grasses, nuts"],
+        diet: ["fruits", "roots", "grasses", "nuts"],
         endangered: false,
     },
     {
         type: "panda bear",
         location: "Asia",
         color: "black and white",
-        diet: ["bamboo shoots, leaves, vegetation, fish"],
+        diet: ["bamboo shoots", "leaves", "vegetation", "fish"],
         endangered: false,
     },
     {
         type: "black bear ",
         location:"Pacific Northwest's forests and mountains ",
         color: " black",
-        diet: ["roots, fish, berries, insects "],
+        diet: ["roots", "fish", "berries", "insects"],
         endangered: false,
     },
 ];
 
+function animal(x){
+    console.log(x[0]);
+    console.log(x[1]);
+    console.log(x[2]);
+    console.log(x[3]);
+    console.log(x[4]);
+}
+
+
 bears.forEach((bear)=> console.log(bear.type));
-bears.forEach((bear)=> console.log(bear.color));
-bears.forEach((bear)=> console.log(bear.diet));
-bears.forEach((bear)=> console.log(bear.endangered));
-const current = bears.filter((bear)=> bear.endangered !== true);
-console.log(current); 
-// const food = bears.filter((bear) => bear.diet !== fish);
-// console.log(food); //THIS ISN'T WORKIG 
+bears.forEach((bear)=>animal(bear.diet));
+
+const food = bears.filter((bear) => bear.endangered !== true);
+console.log(food);
